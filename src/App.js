fb5d4b7fe90d106631848201e93ac41441d9b29a
DivyanshuMasih <divyanshuuk0@gmail.com>
2019-12-01 20:25:24 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React,{Component} from 'react';
+import logo from './logo.svg';
+import './App.css';
+import 'C:/Users/Divyanshu/node_modules/bootstrap/dist/css/bootstrap.css'
+
+
+function App() {
+  return (
+    <div >
+      <span className="App " >Todo
+        </span>
+        <button className="btn btn-secondary btn-sm m-34">+</button>
+        <style>{'body { background-color: #5C615E; }'}</style>
+      </div>
+  );
+}
+
+export default App;
