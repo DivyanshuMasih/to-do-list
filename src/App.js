b59065c74e674020ab5f05ff10f5f7adf4b4151b
DivyanshuMasih <divyanshuuk0@gmail.com>
2019-12-02 16:20:14 +0530

    third commit

--- src/App.js
+++ src/App.js
@@ -3,17 +3,57 @@ import logo from './logo.svg';
 import './App.css';
 import './button.css';
 import 'C:/Users/Divyanshu/node_modules/bootstrap/dist/css/bootstrap.css'
-
-
-function App() {
+import './plus'
+import lists from "./lists"
+class App extends Component{
+  constructor(props)
+  {
+    super(props)
+    
+    this.state =
+    {
+      items:[]
+    };
+    this.addItem = this.addItem.bind(this);
+  }
+  addItem(e)
+  {
+    if(this._inputElement.value !=="")
+    {
+      var newItem = {
+        text:this._inputElement.value,
+        key : Date.now()
+      };
+      this.setState((prevState) => {
+        return {
+          items : prevState.items.concat(newItem)
+        };
+      });
+    }
+    this._inputElement.value = "";
+    console.log(this.state.items);
+    e.preventDefault();
+  }
+render() {
   return (
     <div >
       <span className="App " >Todo
         </span>
-        <button className="button btn btn-secondary btn-sm ">+</button>
+        <div>
+          <form onSubmit={this.addItem}>
+        <button type="submit" className="button btn btn-secondary btn-sm ">+</button>
+        
+          <input ref = {(a) => this._inputElement =a} className="input"></input>
+          </form>
+          </div>
+          <lists entries = {this.state.items}/>
         <style>{'body { background-color: #5C615E; }'}</style>
       </div>
+      
   );
 }
 
+
+  
+}
 export default App;
---
+++ src/plus.js
@@ -0,0 +1,9 @@
+import React,{Component} from 'react';
+class plus extends Component
+{
+ render()
+ {
+
+    return <h1>hello</h1>
+ }   
+}
